05c44bbf1d172fab58cd94a83eaaa79469b55181
JohnObama24 <johnobama24keren@gmail.com>
2024-03-26 23:24:14 +0700

    gant index

--- index.php
+++ index.php
@@ -2,58 +2,108 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>main</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-
-
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>
+    yooooooooooooooooooooo
+  </title>
+  <meta name="description" content="Simple landind page" />
+  <meta name="keywords" content="" />
+  <meta name="author" content="" />
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" />
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet" />
+  <style>
+    .gradient {
+      background: linear-gradient(90deg, #35096b 0%, #990cc8 100%);
+    }
+  </style>
 </head>
-
-<body>
-
-  <div class="flex justify-center items-center">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <img class="w-1/4 h-1/4" src="img/table.png" alt="gambar tabel cuk">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">dashboard</div>
-        <p class="text-gray-700 text-base">
-          untuk melihat data pembeli mkmkmkkmkmm
-        </p>
+<body class="leading-normal tracking-normal text-white gradient" style="font-family: 'Source Sans Pro', sans-serif;">
+  
+  <nav id="header" class="fixed w-full z-30 top-9 ml-2 text-white">
+    <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
+      <div class="pl-4 flex items-center">
+        <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+          <!-- .... -->
+          
+          <!-- logo dan nav -->
+          <svg class="h-8 fill-current inline" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <rect fill="#2a2a31" x="16.539" y="425.626" width="479.767" height="50.502"
+              transform="matrix(1,0,0,1,0,0)" />
+              <path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80v48c0 17.7 14.3 32 32 32s32-14.3 32-32V144C576 64.5 511.5 0 432 0S288 64.5 288 144v48H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H352V144z"/>
+            </svg>
+          MENU UTAMA
+        </a>
       </div>
-      <a href="dashboard.php">
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Dashboard</button>
-      </a>
     </div>
+  </nav>
+  <!-- ...... -->
 
-    <div class="max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <img class="w-1/4 h-1/4" src="img/yt.png" alt="gambar tabel cuk">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Youtbe</div>
-        <p class="text-gray-700 text-base">
-          nonton bg
+    <!-- bagan kiri -->
+    <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+        <p class="uppercase tracking-loose w-full ml-2">Selamat datang Admin!</p>
+        <h1 class="my-4 text-5xl font-bold leading-tight ml-2">
+          GATAU MALES PENGEN BELI TRUK
+        </h1>
+        <p class="ml-2 leading-normal text-2xl mb-8">
+          NYUCI KUY
         </p>
-      </div>    
-      <a href="https://youtu.be/dQw4w9WgXcQ?si=tFpBv8GYiV7WYjdr">
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Youtbe</button>
-      </a>
-    </div>
+        <!-- ..... -->
 
-    <div class="flex justify-center items-center">
-    <div class="max-w-sm rounded overflow-hidden shadow-lg m-4">
-      <img class="w-1/4 h-1/4" src="img/orng.jpg" alt="gambar tabel cuk">
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Contribute</div>
-        <p class="text-gray-700 text-base">
-          yg ada di kelompok ini
-        </p>
+        <!-- Button dashboard, contact, Youtube -->
+        <div class="flex justify-between">
+          <a href="dashboard.php">
+            <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 ml-2 mr-9 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+             Dashboard
+            </button>
+          </a>
+
+          <a href="https://youtu.be/dQw4w9WgXcQ?si=tFpBv8GYiV7WYjdr">
+            <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 mr-9 shadow-lg  focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+              YouTube
+            </button>
+          </a>
+          
+          <a href="credits.php">
+            <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+              Contact
+            </button>
+          </a>
+          
+        </div>
       </div>
-      <a href="credits.php">
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Dashboard</button>
-      </a>
-    </div>
-    </div>
+      <!-- ...... -->
+
+      <!--R=gambar/icon svg-->
+      <div class="w-full md:w-3/5 py-6 text-center">
+        <img class="w-full md:w-4/5 z-50" src="img/nyuci.png" />
+          </div>
+        </div>
+      </div>
+      <!-- ...... -->
+
+      <!-- aliran bawah untuk tambahan -->
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero">
+          <path
+            d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496"
+            opacity="0.100000001"></path>
+          <path
+            d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z"
+            opacity="0.100000001"></path>
+          <path
+            d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z"
+            id="Path-4" opacity="0.200000003"></path>
+        </g>
+        <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero">
+          <path
+            d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z">
+          </path>
+        </g>
+      </g>
+    </svg>
   </div>
 </body>
 
