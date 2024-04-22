4b0e5313332b8acc96198e87faa1aed14e9e2549
JohnObama24 <johnobama24keren@gmail.com>
2024-04-23 02:27:43 +0700

    Memperbarui bagian team

--- credits.php
+++ credits.php
@@ -1,25 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>credit</title>
-    <rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-    <script src="https://cdn.tailwindcss.com"></script>
-    
+    <title>Kredit</title>
+    <!-- Tambahkan link ke CDN Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/d1f6c31734.js" crossorigin="anonymous"></script>
 </head>
-<body>
-<div class="max-w-sm rounded overflow-hidden shadow-lg">
-  <img class="gitw-1/4 h-1/4" src="img/download (1).jpg" alt="orang">
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">John Obama MS </div>
-    <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-    </p>
-  </div>
-  <div class="px-6 pt-4 pb-2">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Main Contributor</span>
-  </div>
-</div>
+
+<!-- body -->
+
+<body class="relative min-h-screen flex justify-center items-center bg-cover bg-no-repeat" style="background-image: url(img/bgr.png);">
+    <!-- --- -->
+
+    <!-- button kembali -->
+    <a href="index.php">
+        <button
+            class="absolute left-6 top-4  hover:bg-purple-900 text-white font-bold py-2 px-4 rounded-lg flex items-center">
+            <i class="fa-solid fa-person-walking-arrow-loop-left mr-2"></i>Beranda
+        </button>
+    </a>
+   
+    <!-- -------- -->
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+        <!-- Kredit Profile Orang Pertama -->
+        <div class="bg-gradient-to-t from-gray-900 to-gray-700 rounded-lg overflow-hidden shadow-md">
+            <img class="m-auto mt-6 w-32 h-32 object-scale-down rounded-full overflow-auto" src="img/soldier.jpg" alt="kobo">
+            <div class="p-4">
+                <h2 class="font-semibold text-white">John Obama Ms</h2>
+                <p class="text-white text-sm mt-2">buat struktur php crud</p>
+                <p class="text-white text-sm mt-2">Deskripsi singkat tentang orang ini.</p>
+            </div>
+        </div>
+        <!-- Kredit Profile Orang Kedua -->
+        <div class="bg-gradient-to-t from-gray-900 to-gray-700 rounded-lg overflow-hidden shadow-md">
+            <img class="m-auto mt-6 w-32 h-32 object-scale-down rounded-full" src="img/iya.jpg" alt="memory">
+            <div class="p-4">
+                <h2 class="font-semibold text-white">M Arridho</h2>
+                <p class="text-white text-sm mt-2">Tampilan/Front-end</p>
+                <p class="text-white text-sm mt-2">Deskripsi singkat tentang orang ini.</p>
+            </div>
+        </div>
+        <!-- Kredit Profile Orang Ketiga -->
+        <div class="bg-gradient-to-t from-gray-900 to-gray-700 rounded-lg overflow-hidden shadow-md">
+            <img class="m-auto mt-6 w-32 h-32 object-scale-down rounded-full" src="img/Gawr.jpg" alt="Gawr">
+            <div class="p-4">
+                <h2 class="font-semibold text-white">Luna aulia Hadi</h2>
+                <p class="text-white text-sm mt-2">Tampilan</p>
+                <p class="text-white text-sm mt-2">Deskripsi singkat tentang orang ini.</p>
+            </div>
+        </div>
+        <!-- Kredit Profile Orang Keempat -->
+        <div class="bg-gradient-to-t from-gray-900 to-gray-700 rounded-lg overflow-hidden shadow-md">
+            <img class="m-auto mt-6 w-32 h-32 object-scale-down rounded-full" src="img/download.jpeg" alt="zeta">
+            <div class="p-4">
+                <h2 class="font-semibold text-white">M fatan Ibrahim</h2>
+                <p class="text-white text-sm mt-2">Prototype</p>
+                <p class="text-white text-sm mt-2">Deskripsi singkat tentang orang ini.</p>
+            </div>
+        </div>
+        <!-- Kredit Profile Orang Kelima -->
+        <div class="bg-gradient-to-t from-gray-900 to-gray-700 rounded-lg overflow-hidden shadow-md">
+            <img class="m-auto mt-6 w-32 h-32 object-scale-down rounded-full" src="img/ayaka.jpg" alt="ayaka">
+            <div class="p-4">
+                <h2 class="font-semibold text-white">M Fizri Ibrahim</h2>
+                <p class="text-white text-sm mt-2">Ntah</p>
+                <p class="text-white text-sm mt-2">Deskripsi singkat tentang orang ini.</p>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -68,7 +68,7 @@
           
           <a href="credits.php">
             <button class="hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
-              Contact
+              TEAM
             </button>
           </a>
           
